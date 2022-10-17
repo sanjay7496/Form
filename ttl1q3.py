a828a48a7e40cd8d4faf50177ba71bace6525611
Saksham Kumar Mishra <64850741+sakshammishra08@users.noreply.github.com>
2022-10-17 09:10:40 +0530

    Q3

---
+++ ttl1q3.py
@@ -0,0 +1,11 @@
+# Python Program to convert temperature in celsius to fahrenheit
+
+celsius=float(input(" Input the Celsius : "))
+
+fahrenheit = (celsius * 1.8) + 32
+print('%0.1f degree Celsius is equal to %0.1f degree Fahrenheit' %(celsius,fahrenheit))
+
+
+#1906272
+#Saksham Kumar Mihsra
+#IT4

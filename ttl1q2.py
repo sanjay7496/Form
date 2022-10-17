64c1f420277e9d1357f55768478317426789d8f0
sanjay7496 <72295803+sanjay7496@users.noreply.github.com>
2022-10-17 09:09:21 +0530

    q2

---
+++ ttl1q2.py
@@ -0,0 +1,20 @@
+l=int(input("Length of Rectangle : "))
+w=int(input("Width of Rectangle : "))
+a=int(input("Length of Aquare : "))
+r=float(input("Radius of Circle : "))
+arear=l*w
+perimeterr=2*(l+w)
+areas=a*a
+perimeters=4*a
+areac=3.14*r*r
+perimeterc=2*3.14*r
+print("Area of Circle : ",areac)
+print("Perimeter of Circle : ",perimeterc)
+print("Area of Square : ",areas)
+print("Perimeter of Square : ",perimeters)
+print("Area of Rectangle : ",arear)
+print("Perimeter of Rectangle : ",perimeterr)
+
+#1906272
+#Saksham Kumar Mihsra
+#IT4
